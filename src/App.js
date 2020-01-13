ad16a0a8203ee7f8ae80c3af48fbac07baba6210
Koichi Miyamoto <m5813k@gmail.com>
2020-01-13 16:59:25 +0900

    Add connection and text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
+  const [gamepad, setGamepad] = useState(null);
+  useEffect(() => {
+    console.log("listen");
+    function connect(e) {
+      console.log("connected", e);
+      setGamepad(e.gamepad);
+    }
+    function disconnect(e) {
+      console.log("disconnected", e);
+      setGamepad(null);
+    }
+    window.addEventListener("gamepadconnected", connect);
+    window.addEventListener("gamepaddisconnected", disconnect);
+    return () => {
+      window.removeEventListener("gamepadconnected", connect);
+      window.removeEventListener("gamepaddisconnected", disconnect);
+    };
+  }, []);
   return (
     <>
       <h1>Hello world</h1>
+      {JSON.stringify(gamepad)}
+      {gamepad?.id}
     </>
   );
 }
